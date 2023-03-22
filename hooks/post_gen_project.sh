b7fd47884663216457449d39615aa9a6dd2f96ec
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2023-03-22 16:10:52 +0000

    feat: update demo to be more real life with string manipulation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 echo "🏁  Starting the cookie cutter script"
-gh repo create {{cookiecutter.github_url}}/{{cookiecutter.github_org}}/{{cookiecutter.repo_name}} --public
+gh repo create {{cookiecutter.github_url}}/{{cookiecutter.github_org}}/{{cookiecutter.project_slug}} --public
 git init
-git remote add origin git@{{cookiecutter.github_url}}:{{cookiecutter.github_org}}/{{cookiecutter.repo_name}}.git
+git remote add origin git@{{cookiecutter.github_url}}:{{cookiecutter.github_org}}/{{cookiecutter.project_slug}}.git
 
 echo "ℹ️  Starting installing the NPM packages"
 npm install
+echo "✅  Starting installing the NPM packages"
+
+echo "ℹ️  Pushing all of the code to Github"
+git add .
+git commit -m "feat: setup of the repository"
+git push origin main
+echo "✅  Code now available at https://{{cookiecutter.github_url}}/{{cookiecutter.github_org}}/{{cookiecutter.project_slug}}"
+
+code .
 
 echo "🚀  Completed the cookie cutter script"
