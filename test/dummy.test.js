38677817cf9641a46ec1c6c0423a9b7749fecfdc
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-06-02 11:35:20 +0100

    chore: second run

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -1,7 +1,7 @@
 const {dummy} = require('../src/dummy');
 
 describe('Validating the dummy', () => {
-    test('should return "This is a dummy"', () => {
+    test('should return the correct value', () => {
         expect(dummy()).toBe('This is a dummy');
     });
 });
